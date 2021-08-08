13b545b76f2da5682f693353f7b9e5f7fd5467a1
dinjonya <123679@qq.com>
2021-08-09 01:07:29 +0800

    🐞 fix(*): template

---
+++ GenerateFilesTemplate/GenerateWebApiTemplate/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GenerateWebApiTemplate.Controllers
+{
+
+    public class HealtController : Controller
+    {
+        [HttpGet]
+        public IActionResult Ok()
+        {
+
+        }
+    }
+}
